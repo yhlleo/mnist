876bba5d5f672ea9888fb461b2341fd900888d26
yhlleo <yhlleo@users.noreply.github.com>
2016-02-19 12:54:31 +0800

    Update mnist_softmax.py

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -1,24 +1,45 @@
-"""A very simple MNIST classifer.
-See extensive documentation at ??????? (insert public URL)
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""A very simple MNIST classifier.
+
+See extensive documentation at
+http://tensorflow.org/tutorials/mnist/beginners/index.md
 """
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
 
 # Import data
+#from tensorflow.examples.tutorials.mnist import input_data
 import input_data
+import tensorflow as tf
+
 mnist = input_data.read_data_sets("Mnist_data/", one_hot=True)
 
-import tensorflow as tf
 sess = tf.InteractiveSession()
 
 # Create the model
-x = tf.placeholder("float", [None, 784])
-W = tf.Variable(tf.zeros([784,10]))
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(x,W) + b)
+y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 # Define loss and optimizer
-y_ = tf.placeholder("float", [None,10])
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 # Train
@@ -28,6 +49,6 @@ for i in range(1000):
   train_step.run({x: batch_xs, y_: batch_ys})
 
 # Test trained model
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
