343781cd2eef35d38deebc14ac1c6c76af617270
yhlleo <yhlleo@users.noreply.github.com>
2016-02-19 13:55:47 +0800

    Update fully_connected_feed.py

--- fully_connected_feed.py
+++ fully_connected_feed.py
@@ -35,7 +35,7 @@ import input_data, mnist
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
-flags.DEFINE_integer('max_steps', 5000, 'Number of steps to run trainer.')
+flags.DEFINE_integer('max_steps', 2000, 'Number of steps to run trainer.')
 flags.DEFINE_integer('hidden1', 128, 'Number of units in hidden layer 1.')
 flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
 flags.DEFINE_integer('batch_size', 100, 'Batch size.  '
